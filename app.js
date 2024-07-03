1ed9b8d54ac4041f4c8673276bab0e1b6797f5c0
Nour Bahar <nourbahar@gmail.com>
2024-07-03 08:55:54 +0300

    AG

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 
 /*-------------------------------- Variables --------------------------------*/
 
-let FirstValue
-let SecondValue
-let operator
+let FirstValue = null
+let SecondValue = null
+let operator = null
+let result 
 /*------------------------ Cached Element References ------------------------*/
 const buttons = document.querySelectorAll('.button');
 const calculator = document.querySelector('#calculator');
@@ -12,61 +13,78 @@ const display = document.querySelector('.display');
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-
-calculator.addEventListener('click', (event) => {
-    
-    //console.log(event.target.innerText);
-  
-  
-    if (event.target.classList.contains('number')) {
-      
-    }
-  
-   else if (event.target.innerText === '*') {
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
      
-    }
-
-   else if (event.target.innerText === '-') {
-     
-
-    }
-   else if (event.target.innerText === '+') {
-     
-
-    }
-   else if (event.target.innerText === '=') {
-        
-    }
-
+    });
   });
 
 
 
-calculator.addEventListener('click', (event) => {
-    if (event.target.classList.contains('C')) {
-       
-        display.innerText = '0';
-        
+  calculator.addEventListener('click', (event) => {
+  
+
+    if (event.target.classList.contains('number')) {
+      inputNumber(event.target.innerText)
     }
-});
+    if (event.target.classList.contains('operator')){
+        if(operator===null){
+      operator=event.target.innerText
+      display.innerText = operator
+        }
+    }
+    if(event.target.classList.contains('equals'))
+        {
+            calculate()
+        }
+
+        if(event.target.classList.contains('clear'))
+            {
+                clear()
+            }
+  });
 /*-------------------------------- Functions --------------------------------*/
 
+function inputNumber(value){
+    if(FirstValue === null)
+        { FirstValue=value}
+    else if (operator === null)
+        {FirstValue+=value}
+    else if (SecondValue === null)
+        {SecondValue=value}
+    else {SecondValue+=value}
+display.innerText+=value
+}
 
-function calculate(operator, FirstValue,SecondValue ) {
-    switch(operator) {
-        case '+':
-            return FirstValue + SecondValue;
-        case '-':
-            return FirstValue - SecondValue;
-        case '*':
-            return FirstValue * SecondValue;
-        case '/':
-            if (FirstValue === 0) {
-                return ' '; 
-            }
-            return FirstValue / SecondValue;
-        
-    }
+function calculate() {
+    if(FirstValue && SecondValue && operator){
+        const num1 = parseFloat(FirstValue)
+        const num2 = parseFloat(SecondValue)
+     if (operator === '+'){
+        result = num1 + num2
+     }
+     else if (operator === '-'){
+        result = num1 - num2
+     }
+     else if (operator === '*'){
+        result = num1 * num2
+     }
+
+     else if (operator === '/'){
+        result = num1 / num2
+     }
+
+display.innerText= result
+
+}
+}
+
+function clear(){
+    FirstValue= null
+    SecondValue= null
+    operator= null
+    result=null
+    display.innerText=''
 }
 
 
